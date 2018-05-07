429520821c85f3e711d047410df7735bf1f00b19
Edwin Siebel <edwin@yard.nl>
2018-05-07 11:22:11 +0200

    Fix: Config

--- src/Elasticsearch/Config.php
+++ src/Elasticsearch/Config.php
@@ -5,196 +5,185 @@ namespace OWC\Elasticsearch;
 class Config
 {
 
-	/**
-	 * Directory where config files are located.
-	 *
-	 * @var string
-	 */
-	protected $path;
+    /**
+     * Directory where config files are located.
+     *
+     * @var string
+     */
+    protected $path;
 
-	/**
-	 * Array with names of protected nodes in the config-items.
-	 *
-	 * @var array
-	 */
-	protected $protectedNodes = [];
+    /**
+     * Array with names of protected nodes in the config-items.
+     *
+     * @var array
+     */
+    protected $protectedNodes = [];
 
-	/**
-	 * Array with all the config values.
-	 *
-	 * @var array
-	 */
-	protected $items = [];
+    /**
+     * Array with all the config values.
+     *
+     * @var array
+     */
+    protected $items = [];
 
-	/**
-	 * Config repository constructor.
-	 *
-	 * Boot the configuration files and get all the files from the
-	 * config directory and add them to the config array.
-	 *
-	 * @param string $path Path to the configuration files.
-	 * @param array  $items
-	 */
-	public function __construct($path, array $items = [])
-	{
-		$this->items = $items;
-		$this->path  = $path;
-	}
+    /**
+     * Config repository constructor.
+     *
+     * Boot the configuration files and get all the files from the
+     * config directory and add them to the config array.
+     *
+     * @param string $path Path to the configuration files.
+     * @param array  $items
+     */
+    public function __construct($path, array $items = [])
+    {
+        $this->items = $items;
+        $this->path  = $path;
+    }
 
-	/**
-	 * Boot up the configuration repository.
-	 */
-	public function boot()
-	{
-		$this->scanDirectory($this->getPath());
-	}
+    /**
+     * Boot up the configuration repository.
+     */
+    public function boot()
+    {
+        $this->scanDirectory($this->getPath());
+    }
 
-	/**
-	 * Retrieve a specific config value from the configuration repository.
-	 *
-	 * @param $setting
-	 *
-	 * @return array|mixed
-	 */
-	public function get($setting)
-	{
-		if ( ! $setting ) {
-			return $this->all();
-		}
+    /**
+     * Retrieve a specific config value from the configuration repository.
+     *
+     * @param $setting
+     *
+     * @return array|mixed
+     */
+    public function get($setting)
+    {
+        if ( ! $setting ) {
+            return $this->all();
+        }
 
-		$parts = explode('.', $setting);
+        $parts = explode('.', $setting);
 
-		$current = $this->items;
+        $current = $this->items;
 
-		foreach ( $parts as $part ) {
-			$current = $current[ $part ];
-		}
+        foreach ( $parts as $part ) {
+            $current = $current[ $part ];
+        }
 
-		return $current;
-	}
+        return $current;
+    }
 
-	/**
-	 * Set a given configuration value.
-	 *
-	 * @param  array|string $key
-	 * @param  mixed        $value
-	 *
-	 * @return void
-	 */
-	public function set($key, $value = null)
-	{
-		$keys = is_array($key) ? $key : [$key => $value];
+    /**
+     * Set a given configuration value.
+     *
+     * @param  array|string $key
+     * @param  mixed        $value
+     *
+     * @return void
+     */
+    public function set($key, $value = null)
+    {
+        $keys = is_array($key) ? $key : [$key => $value];
 
-		$tempItems = &$this->items;
+        $tempItems = &$this->items;
 
-		foreach ( $keys as $key => $value ) {
+        foreach ( $keys as $key => $value ) {
 
-			if ( in_array($key, $this->protectedNodes) ) {
-				continue;
-			}
+            if ( in_array($key, $this->protectedNodes) ) {
+                continue;
+            }
 
-			$parts = explode('.', $key);
-			while ( count($parts) > 1 ) {
+            $parts = explode('.', $key);
+            while ( count($parts) > 1 ) {
 
-				$part = array_shift($parts);
-				// If the key doesn't exist at this depth, we will just create an empty array
-				// to hold the next value, allowing us to create the arrays to hold final
-				// values at the correct depth. Then we'll keep digging into the array.
-				if ( ! isset($tempItems[ $part ]) || ! is_array($tempItems[ $part ]) ) {
-					$tempItems[ $part ] = [];
-				}
-				$tempItems = &$tempItems[ $part ];
-			}
+                $part = array_shift($parts);
+                // If the key doesn't exist at this depth, we will just create an empty array
+                // to hold the next value, allowing us to create the arrays to hold final
+                // values at the correct depth. Then we'll keep digging into the array.
+                if ( ! isset($tempItems[ $part ]) || ! is_array($tempItems[ $part ]) ) {
+                    $tempItems[ $part ] = [];
+                }
+                $tempItems = &$tempItems[ $part ];
+            }
 
-			$tempItems[ array_shift($parts) ] = $value;
-		}
-	}
+            $tempItems[ array_shift($parts) ] = $value;
+        }
+    }
 
-	/**
-	 * Return all config values.
-	 *
-	 * @return array
-	 */
-	public function all()
-	{
-		return $this->items;
-	}
+    /**
+     * Return all config values.
+     *
+     * @return array
+     */
+    public function all()
+    {
+        return $this->items;
+    }
 
-	/**
-	 * Get the path where the files will be fetched from.
-	 *
-	 * @return string
-	 */
-	public function getPath()
-	{
-		return $this->path;
-	}
+    /**
+     * Get the path where the files will be fetched from.
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
 
-	/**
-	 * Sets the path where the config files are fetched from.
-	 *
-	 * @param $path
-	 */
-	public function setPath($path)
-	{
-		$this->path = $path;
-	}
+    /**
+     * Sets the path where the config files are fetched from.
+     *
+     * @param $path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
 
-	public function setProtectedNodes($nodes = [])
-	{
-		$this->protectedNodes = $nodes;
-	}
+    /**
+     * @param array $nodes
+     */
+    public function setProtectedNodes($nodes = [])
+    {
+        $this->protectedNodes = $nodes;
+    }
 
-	/**
-	 * Push a value onto an array configuration value.
-	 *
-	 * @param  string $key
-	 * @param  mixed  $value
-	 *
-	 * @return void
-	 */
-	public function push($key, $value)
-	{
-		$array   = $this->get($key);
-		$array[] = $value;
-		$this->set($key, $array);
-	}
+    /**
+     * @param $path
+     */
+    private function scanDirectory($path)
+    {
+        $files = glob($path . '/*', GLOB_NOSORT);
 
-	private function scanDirectory($path)
-	{
-		$this->items = [];
+        foreach ( $files as $file ) {
 
-		$files = glob($path . '/*', GLOB_NOSORT);
+            $fileType = filetype($file);
 
-		foreach ( $files as $file ) {
+            if ( $fileType == "dir" ) {
+                $this->scanDirectory($file);
+            } else {
+                $name  = str_replace('.php', '', basename($file));
+                $value = include $file;
 
-			$fileType = filetype($file);
+                // If its in the first directory just add the file.
+                if ( $path == $this->path ) {
+                    $this->items[ $name ] = $value;
+                    continue;
+                }
 
-			if ( $fileType == "dir" ) {
-				$this->scanDirectory($file);
-			} else {
-				$name  = str_replace('.php', '', basename($file));
-				$value = include $file;
+                // Get the path from the starting path.
+                $path = str_replace($this->path . '/', '', $path);
 
-				// If its in the first directory just add the file.
-				if ( $path == $this->path ) {
-					$this->items[ $name ] = $value;
-					continue;
-				}
+                // Build an array from the path.
+                $items          = [];
+                $items[ $name ] = $value;
+                foreach ( array_reverse(explode('/', $path)) as $key ) {
+                    $items = [$key => $items];
+                }
 
-				// Get the path from the starting path.
-				$path = str_replace($this->path . '/', '', $path);
-
-				// Build an array from the path.
-				$items          = [];
-				$items[ $name ] = $value;
-				foreach ( array_reverse(explode('/', $path)) as $key ) {
-					$items = [$key => $items];
-				}
-
-				// Merge it recursively into items
-				$this->items = array_merge_recursive($this->items, $items);
-			}
-		}
-	}
+                // Merge it recursively into items
+                $this->items = array_merge_recursive($this->items, $items);
+            }
+        }
+    }
 }
--- tests/Unit/Config/RepositoryTest.php
+++ tests/Unit/Config/RepositoryTest.php
@@ -8,276 +8,279 @@ use OWC\Elasticsearch\Tests\TestCase;
 class RepositoryTest extends TestCase
 {
 
-	/**
-	 * @var \OWC\Elasticsearch\Config
-	 */
-	protected $repository;
+    public function setUp()
+    {
+        \WP_Mock::setUp();
+    }
 
-	/**
-	 * @var array
-	 */
-	protected $config;
+    public function tearDown()
+    {
+        \WP_Mock::tearDown();
+    }
 
-	public function setUp()
-	{
-		\WP_Mock::setUp();
+    /** @test */
+    public function gets_value_correctly()
+    {
+        $repository = new Config(__DIR__ . '/config');
+        $repository->boot();
 
-		$this->repository = new Config(__DIR__ . '/config');
-	}
+        $config = [
+            'test'      => [
+                'single_file' => true
+            ],
+            'directory' => [
+                'testfile' => [
+                    'in_directory' => 'directory'
+                ],
+                'multi'     => [
+                    'deep' => [
+                        'multi_level' => 'works'
+                    ]
+                ]
+            ]
+        ];
 
-	public function tearDown()
-	{
-		\WP_Mock::tearDown();
-	}
+        $this->assertEquals($config, $repository->all());
+        $this->assertEquals($config, $repository->get(false));
+        $this->assertEquals(true, $repository->get('test.single_file'));
+        $this->assertEquals('directory', $repository->get('directory.testfile.in_directory'));
+        $this->assertEquals('works', $repository->get('directory.multi.deep.multi_level'));
+    }
 
-	/** @test */
-	public function gets_value_correctly()
-	{
-		$this->repository->boot();
+    /** @test */
+    public function check_setting_of_path()
+    {
 
-		$config = [
-			'test'      => [
-				'single_file' => true
-			],
-			'directory' => [
-				'testfile' => [
-					'in_directory' => 'directory',
-				],
-				'multi'    => [
-					'deep' => [
-						'multi_level' => 'works'
-					]
-				]
-			]
-		];
+        $repository = new Config(__DIR__ . '/config');
+        $path       = '/test/path/config/';
+        $repository->setPath($path);
 
-		$this->assertEquals($config, $this->repository->all());
-		$this->assertEquals($config, $this->repository->get(false));
-		$this->assertEquals(true, $this->repository->get('test.single_file'));
-		$this->assertEquals('directory', $this->repository->get('directory.testfile.in_directory'));
-		$this->assertEquals('works', $this->repository->get('directory.multi.deep.multi_level'));
-	}
+        $this->assertEquals($repository->getPath(), $path);
+    }
 
-	/** @test */
-	public function check_setting_of_path()
-	{
+    /** @test */
+    public function check_setting_of_protected_nodes()
+    {
+        $repository = new Config(__DIR__ . '/config');
+        $repository->boot();
 
-		$path = '/test/path/config/';
-		$this->repository->setPath($path);
+        $expectedConfig = [
+            'test'      => [
+                'test'
+            ],
+            'directory' => [
+                'testfile' => [
+                    'in_directory' => 'directory'
+                ],
+                'multi'     => [
+                    'deep' => [
+                        'multi_level' => 'works'
+                    ]
+                ]
+            ]
+        ];
+        $repository->set('test', ['test']);
+        $this->assertEquals($expectedConfig, $repository->all());
 
-		$this->assertEquals($this->repository->getPath(), $path);
-	}
+        $repository->setProtectedNodes(['test']);
+        $repository->set('test', ['test2']);
+        $this->assertEquals($expectedConfig, $repository->all());
 
-	/** @test */
-	public function check_setting_of_protected_nodes()
-	{
-		$this->repository->boot();
+        $expectedConfig = [
+            'test'      => [
+                'single_file' => true
+            ],
+            'directory' => 'test'
+        ];
 
-		$expectedConfig = [
-			'test'      => [
-				'test'
-			],
-			'directory' => [
-				'testfile' => [
-					'in_directory' => 'directory',
-				],
-				'multi'    => [
-					'deep' => [
-						'multi_level' => 'works'
-					]
-				]
-			]
-		];
-		$this->repository->set('test', ['test']);
-		$this->assertEquals($expectedConfig, $this->repository->all());
+        $repository = new Config(__DIR__ . '/config');
+        $repository->boot();
+        $repository->set('directory', 'test');
+        $this->assertEquals($expectedConfig, $repository->all());
 
-		$this->repository->setProtectedNodes(['test']);
-		$this->repository->set('test', ['test2']);
-		$this->assertEquals($expectedConfig, $this->repository->all());
+        $expectedConfig = [
+            'test'      => [
+                'single_file' => true
+            ],
+            'directory' => [
+                'test' => 'node'
+            ]
+        ];
+        $repository->set('directory', ['test' => 'node']);
+        $this->assertEquals($expectedConfig, $repository->all());
 
-		$expectedConfig = [
-			'test'      => [
-				'single_file' => true
-			],
-			'directory' => 'test'
-		];
-		$this->repository->boot();
-		$this->repository->set('directory', 'test');
-		$this->assertEquals($expectedConfig, $this->repository->all());
+        $expectedConfig = [
+            'test'      => [
+                'single_file' => true
+            ],
+            'directory' => [
+                'test' => [
+                    'node' => 'nog deeper'
+                ]
+            ]
+        ];
+        $repository->set('directory', ['test' => ['node' => 'nog deeper']]);
+        $this->assertEquals($expectedConfig, $repository->all());
 
-		$expectedConfig = [
-			'test'      => [
-				'single_file' => true
-			],
-			'directory' => [
-				'test' => 'node'
-			]
-		];
-		$this->repository->set('directory', ['test' => 'node']);
-		$this->assertEquals($expectedConfig, $this->repository->all());
+        $expectedConfig = [
+            'test'      => [
+                'single_file' => true
+            ],
+            'directory' => [
+                'testfile' => 'test',
+                'multi'     => [
+                    'deep' => [
+                        'multi_level' => 'works'
+                    ]
+                ]
+            ]
+        ];
 
-		$expectedConfig = [
-			'test'      => [
-				'single_file' => true
-			],
-			'directory' => [
-				'test' => [
-					'node' => 'nog deeper'
-				]
-			]
-		];
-		$this->repository->set('directory', ['test' => ['node' => 'nog deeper']]);
-		$this->assertEquals($expectedConfig, $this->repository->all());
+        $repository = new Config(__DIR__ . '/config');
+        $repository->boot();
+        $repository->set('directory.testfile', 'test');
+        $this->assertEquals($expectedConfig, $repository->all());
 
-		$expectedConfig = [
-			'test'      => [
-				'single_file' => true
-			],
-			'directory' => [
-				'testfile' => 'test',
-				'multi'    => [
-					'deep' => [
-						'multi_level' => 'works'
-					]
-				]
-			]
-		];
-		$this->repository->boot();
-		$this->repository->set('directory.testfile', 'test');
-		$this->assertEquals($expectedConfig, $this->repository->all());
+        $expectedConfig = [
+            'test'      => [
+                'single_file' => true
+            ],
+            'directory' => [
+                'testfile' => [
+                    'test' => 'node'
+                ],
+                'multi'     => [
+                    'deep' => [
+                        'multi_level' => 'works'
+                    ]
+                ]
+            ]
+        ];
+        $repository->set('directory.testfile', ['test' => 'node']);
+        $this->assertEquals($expectedConfig, $repository->all());
 
-		$expectedConfig = [
-			'test'      => [
-				'single_file' => true
-			],
-			'directory' => [
-				'testfile' => [
-					'test' => 'node'
-				],
-				'multi'    => [
-					'deep' => [
-						'multi_level' => 'works'
-					]
-				]
-			]
-		];
-		$this->repository->set('directory.testfile', ['test' => 'node']);
-		$this->assertEquals($expectedConfig, $this->repository->all());
+        $expectedConfig = [
+            'test'      => [
+                'single_file' => true
+            ],
+            'directory' => [
+                'testfile' => [
+                    'test' => [
+                        'node' => 'nog deeper'
+                    ]
+                ],
+                'multi'     => [
+                    'deep' => [
+                        'multi_level' => 'works'
+                    ]
+                ]
+            ]
+        ];
+        $repository->set('directory.testfile', ['test' => ['node' => 'nog deeper']]);
+        $this->assertEquals($expectedConfig, $repository->all());
 
-		$expectedConfig = [
-			'test'      => [
-				'single_file' => true
-			],
-			'directory' => [
-				'testfile' => [
-					'test' => [
-						'node' => 'nog deeper'
-					]
-				],
-				'multi'    => [
-					'deep' => [
-						'multi_level' => 'works'
-					]
-				]
-			]
-		];
-		$this->repository->set('directory.testfile', ['test' => ['node' => 'nog deeper']]);
-		$this->assertEquals($expectedConfig, $this->repository->all());
+        $expectedConfig = [
+            'test'      => [
+                'single_file' => true
+            ],
+            'directory' => [
+                'testfile' => [
+                    'in_directory' => 'directory',
+                ],
+                'multi'     => 'test'
+            ]
+        ];
 
-		$expectedConfig = [
-			'test'      => [
-				'single_file' => true
-			],
-			'directory' => [
-				'testfile' => [
-					'in_directory' => 'directory',
-				],
-				'multi'    => 'test'
-			]
-		];
-		$this->repository->boot();
-		$this->repository->set('directory.multi', 'test');
-		$this->assertEquals($expectedConfig, $this->repository->all());
+        $repository = new Config(__DIR__ . '/config');
+        $repository->boot();
+        $repository->set('directory.multi', 'test');
+        $this->assertEquals($expectedConfig, $repository->all());
 
-		$expectedConfig = [
-			'test'      => [
-				'single_file' => true
-			],
-			'directory' => [
-				'testfile' => [
-					'in_directory' => 'directory',
-				],
-				'multi'    => [
-					'deep' => 'test'
-				]
-			]
-		];
-		$this->repository->boot();
-		$this->repository->set('directory.multi.deep', 'test');
-		$this->assertEquals($expectedConfig, $this->repository->all());
+        $expectedConfig = [
+            'test'      => [
+                'single_file' => true
+            ],
+            'directory' => [
+                'testfile' => [
+                    'in_directory' => 'directory',
+                ],
+                'multi'     => [
+                    'deep' => 'test'
+                ]
+            ]
+        ];
 
-		$expectedConfig = [
-			'test'      => [
-				'single_file' => true
-			],
-			'directory' => [
-				'testfile' => [
-					'in_directory' => 'directory',
-				],
-				'multi'    => [
-					'deep' => [
-						'multi_level' => 'works_also_via_set'
-					]
-				]
-			]
-		];
-		$this->repository->set('directory.multi.deep', ['multi_level' => 'works_also_via_set']);
-		$this->assertEquals($expectedConfig, $this->repository->all());
+        $repository = new Config(__DIR__ . '/config');
+        $repository->boot();
+        $repository->set('directory.multi.deep', 'test');
+        $this->assertEquals($expectedConfig, $repository->all());
 
-		$expectedConfig = [
-			'test'         => [
-				'single_file' => true
-			],
-			'directory'    => [
-				'testfile' => [
-					'in_directory' => 'directory',
-				],
-				'multi'    => [
-					'deep' => [
-						'multi_level' => 'works'
-					]
-				]
-			],
-			'doesnotexist' => [
-				'directory' => [
-					'multi' => [
-						'deep' => null
-					]
-				]
-			]
-		];
-		$this->repository->boot();
-		$this->repository->set('doesnotexist.directory.multi.deep');
-		$this->assertEquals($expectedConfig, $this->repository->all());
+        $expectedConfig = [
+            'test'      => [
+                'single_file' => true
+            ],
+            'directory' => [
+                'testfile' => [
+                    'in_directory' => 'directory',
+                ],
+                'multi'     => [
+                    'deep' => [
+                        'multi_level' => 'works_also_via_set'
+                    ]
+                ]
+            ]
+        ];
+        $repository->set('directory.multi.deep', ['multi_level' => 'works_also_via_set']);
+        $this->assertEquals($expectedConfig, $repository->all());
 
-		$expectedConfig = [
-			'test'      => [
-				'single_file' => true
-			],
-			'directory' => [
-				'testfile' => [
-					'in_directory' => 'directory',
-				],
-				'multi'    => [
-					'deep' => [
-						'multi_level' => 'works'
-					]
-				]
-			],
-			''          => null
-		];
-		$this->repository->boot();
-		$this->repository->set([null => null]);
-		$this->assertEquals($expectedConfig, $this->repository->all());
-	}
+        $expectedConfig = [
+            'test'         => [
+                'single_file' => true
+            ],
+            'directory'    => [
+                'testfile' => [
+                    'in_directory' => 'directory',
+                ],
+                'multi'     => [
+                    'deep' => [
+                        'multi_level' => 'works'
+                    ]
+                ]
+            ],
+            'doesnotexist' => [
+                'directory' => [
+                    'multi' => [
+                        'deep' => null
+                    ]
+                ]
+            ]
+        ];
+
+        $repository = new Config(__DIR__ . '/config');
+        $repository->boot();
+        $repository->set('doesnotexist.directory.multi.deep');
+        $this->assertEquals($expectedConfig, $repository->all());
+
+        $expectedConfig = [
+            'test'      => [
+                'single_file' => true
+            ],
+            'directory' => [
+                'testfile' => [
+                    'in_directory' => 'directory',
+                ],
+                'multi'     => [
+                    'deep' => [
+                        'multi_level' => 'works'
+                    ]
+                ]
+            ],
+            ''          => null
+        ];
+
+        $repository = new Config(__DIR__ . '/config');
+        $repository->boot();
+        $repository->set([null => null]);
+        $this->assertEquals($expectedConfig, $repository->all());
+    }
 }
